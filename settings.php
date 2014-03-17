06637c6f7f8f1c77d6c155c87ed080931b7c6081
manuel <sedras@gmail.com>
2014-03-17 17:40:22 +0100

    New setting: secretaria W.S

--- settings.php
+++ settings.php
@@ -1,4 +1,28 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Secretaria plugin: settings
+ *
+ * @package    local
+ * @subpackage secretaria
+ * @copyright  Institut Obert de Catalunya (IOC) 2014
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -10,6 +34,21 @@ if ($hassiteconfig) {
     $settings->add(new admin_setting_configselect('local_secretaria/auth_plugin',
                                                   get_string('auth_plugin', 'local_secretaria'), '',
                                                   'manual', $options));
+
+
+    $settings->add(new admin_setting_heading('secretariaws', get_string('ws', 'local_secretaria')));
+
+
+    $settings->add(new admin_setting_configtext('local_secretaria/courses',
+                                            get_string('courses', 'local_secretaria'), '', ''));
+    $settings->add(new admin_setting_configtext('local_secretaria/secretarias',
+                                            get_string('secretarias', 'local_secretaria'), '', ''));
+
+    $settings->add(new admin_setting_configtext('local_secretaria/password',
+                                            get_string('password', 'local_secretaria'), '', ''));
+
+    $settings->add(new admin_setting_configtext('local_secretaria/method',
+                                            get_string('method', 'local_secretaria'), '', ''));
 
     $ADMIN->add('localplugins', $settings);
 }
