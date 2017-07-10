5ba1588412e3b1b6194cdbe727f3b5790a8b0379
Marc Català <mcatala@itteria.cat>
2017-07-10 11:49:06 +0200

    Ús de la nova funció image_url

--- mailcheck.php
+++ mailcheck.php
@@ -1,4 +1,18 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 require_once(dirname(__FILE__) . '/../../config.php');
 require_once($CFG->libdir . '/adminlib.php');
@@ -56,9 +70,9 @@ if (!empty($CFG->local_xtecmail_app)) {
             $output .= html_writer::start_div();
             $output .= get_string('status', 'moodle') . ': ';
             if ($xm->test()) {
-                $output .= html_writer::empty_tag('img', array('src' => $OUTPUT->pix_url('t/check')));
+                $output .= html_writer::empty_tag('img', array('src' => $OUTPUT->image_url('t/check')));
             } else {
-                $output .= html_writer::empty_tag('img', array('src' => $OUTPUT->pix_url('i/warning')));
+                $output .= html_writer::empty_tag('img', array('src' => $OUTPUT->image_url('i/warning')));
             }
             $output .= html_writer::end_div();
         } catch (xtecmailerror $e) {
